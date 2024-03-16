677b77f39550e1431a40c55a5a3aa4343ca507b8
divmecool14 <Divmecool16@gmail.com>
2024-03-16 00:19:16 +0000

    hello

---
+++ src/main/java/com/mycompany/music/app/MusicApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.music.app;
+
+/**
+ *
+ * @author divme
+ */
+public class MusicApp {
+
+    public static void main(String[] args) {
+        Home home = new Home();
+        
+    }
+}
